95749baf62d097206c574c2a429700de22612c17
jinke <1359518268@qq.com>
2017-12-25 18:37:33 +0800

    feat:add html

---
+++ src/config.js
@@ -0,0 +1,31 @@
+export const fontFamily = [
+    {
+        label: "微软雅黑",
+        value: "Microsoft YaHei"
+    },
+    {
+        label: "海维提卡",
+        value: "Helvetica"
+    }
+]
+
+export const imageProcess = [
+    {
+        label: "系统默认",
+        value: "default"
+    },
+    {
+        label: "翻转",
+        value: "reversal"
+    },
+    {
+        label: "压缩",
+        value: "compress"
+    }
+]
+
+export const fontSize = Array.from({ length: 60 }).map((_, i) => i+1).filter(v => v > 15)
+
+export const defaultFontSize = fontSize[0]
+
+export const defaultFontText = "测试默认文字"
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React, { PureComponent } from "react"
 import Container from "./components/Container"
-import { Button, Divider, Col, Row, Form, Input, Modal, message } from "antd"
+import { Button, Divider, Col, Row, Form, Input, Modal, message, Select,Slider } from "antd"
+import { SketchPicker } from 'react-color'
+import { fontFamily, defaultFontText,imageProcess,defaultFontSize,fontSize } from "./config"
 
 const { FormItem } = Form
+const { Option } = Select
+const { TextArea } = Input
+
 const prefix = 'react-meme'
 
 class ReactMeme extends PureComponent {
     state = {
-        cameraVisible: false
+        cameraVisible: false,
+        displayColorPicker: false,
+        fontColor:"#444",
+        defaultFontSize
     }
     constructor(props) {
         super(props)
     }
+    static defaultProps = {
+        defaultFont: fontFamily[0].value,
+        defaultImageProcess:imageProcess[0].value,
+        defaultText: defaultFontText,
+        defaultFontSize
+    }
+    toggleColorPicker = () => {
+        this.setState({ displayColorPicker: !this.state.displayColorPicker })
+    }
+    closeColorPircker = () => {
+        this.setState({ displayColorPicker: false })
+    }
+    colorChange = ({ hex }) => {
+        this.setState({ fontColor: hex })
+    };
     createMeme = (e) => {
         e.preventDefault()
         this.props.form.validateFields((err, values) => {
@@ -21,28 +44,28 @@ class ReactMeme extends PureComponent {
         })
     }
     openCamera = () => {
-        if (navigator.mediaDevices &&  navigator.mediaDevices.getUserMedia) {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({
                 video: true,
                 audio: true
             })
-            .then((data) => {
-                const cameraUrl = window.URL.createObjectURL(data)
-                this.setState({
-                    // cameraUrl,
-                    cameraVisible: true
+                .then((data) => {
+                    const cameraUrl = window.URL.createObjectURL(data)
+                    this.setState({
+                        // cameraUrl,
+                        cameraVisible: true
+                    })
                 })
-            })
-            .catch((error) => {
-                Modal.error({
-                    title: "调用摄像头失败",
-                    content: error
+                .catch((error) => {
+                    Modal.error({
+                        title: "调用摄像头失败",
+                        content: error
+                    })
+                    this.setState({ cameraVisible: false })
                 })
-                this.setState({cameraVisible: false})
-            })
         } else {
-            Modal.error({title: '抱歉,你的电脑暂不支持摄像头!'})
-            this.setState({cameraVisible: false})
+            Modal.error({ title: '抱歉,你的电脑暂不支持摄像头!' })
+            this.setState({ cameraVisible: false })
         }
         // this.setState({ cameraVisible: true })
 
@@ -50,8 +73,11 @@ class ReactMeme extends PureComponent {
     closeCamera = () => {
         this.setState({ cameraVisible: false })
     }
+    fontSizeChange = (value)=>{
+        console.log(value);
+    }
     //截取当前摄像头 帧
-    screenShotCamera = ()=>{
+    screenShotCamera = () => {
 
     }
     render() {
@@ -66,8 +92,28 @@ class ReactMeme extends PureComponent {
 
         const {
             cameraVisible,
-            cameraUrl
+            cameraUrl,
+            fontColor,
+            displayColorPicker
         } = this.state
+
+        const {
+            defaultFont,
+            defaultFontSize,
+            defaultImageProcess,
+            defaultText
+        } = this.props
+
+        const labelSpan = 4
+        const valueSpan = 19
+        const offsetSpan = 1
+
+        const operationRow = ({ icon = "edit" ,label, component }) => (
+            <Row className={`${prefix}-item`}>
+                <Col span={labelSpan} className={`${prefix}-item-label`}><Button type="primary" icon={icon}>{label}</Button></Col>
+                <Col span={valueSpan} offset={offsetSpan} className={`${prefix}-item-input`}>{component}</Col>
+            </Row>
+        )
 
         return (
             <Container className={prefix}>
@@ -75,10 +121,100 @@ class ReactMeme extends PureComponent {
                 <section className={`${prefix}-main`}>
                     <Row>
                         <Col span="8">
-                            <Button type="primary" onClick={this.openCamera}>使用摄像头</Button>
+                            <div className="preview-content">
+                                
+                            </div>
+                            <Button type="primary">选择图片</Button>
                         </Col>
                         <Col span="16">
+                            {
+                                operationRow({
+                                    label: '文字',
+                                    component: (
+                                        <TextArea
+                                            autosize={true}
+                                            defaultValue={defaultText}
+                                        />
+                                    )
+                                })
+                            }
+                            {
+                                operationRow({
+                                    icon:"file-ppt",
+                                    label: '字体',
+                                    component: (
+                                        <Select style={{ width: "100%" }} defaultValue={defaultFont}>
+                                            {
+                                                fontFamily.map(({ label, value }, i) => (
+                                                    <Option value={value} key={i}>{label}</Option>
+                                                ))
+                                            }
+                                        </Select>
+                                    )
+                                })
+                            }
+                            {
+                                operationRow({
+                                    icon:"pie-chart",
+                                    label: '颜色',
+                                    component: (
+                                        <div>
+                                            <div className="color-section" onClick={this.toggleColorPicker}>
+                                                <div 
+                                                    className="color" 
+                                                    style={{
+                                                    background:fontColor
+                                                    }}
+                                                />
+                                            </div>
+                                            {
+                                                displayColorPicker 
+                                                ? <div className="popover">
+                                                    <div className="cover" onClick={this.closeColorPircker} />
+                                                    <SketchPicker 
+                                                        color={fontColor} 
+                                                        onChange={this.colorChange} 
+                                                    />
+                                                </div> 
+                                                : undefined
 
+                                            }
+                                        </div>
+                                    )
+                                })
+                            }
+                            {
+                                operationRow({
+                                    icon:"picture",
+                                    label: '图片处理',
+                                    component: (
+                                        <Select style={{ width: "100%" }} defaultValue={defaultImageProcess}>
+                                            {
+                                                imageProcess.map(({ label, value }, i) => (
+                                                    <Option value={value} key={i}>{label}</Option>
+                                                ))
+                                            }
+                                        </Select>
+                                    )
+                                })
+                            }
+                            {
+                                operationRow({
+                                    icon:"line-chart",
+                                    label: '文字大小',
+                                    component: (
+                                        <Slider 
+                                            min={fontSize[0]}
+                                            max={fontSize[fontSize.length-1]}
+                                            defaultValue={defaultFontSize} 
+                                            onAfterChange={this.fontSizeChange}
+                                        />
+                                    )
+                                })
+                            }
+                            <Row>
+                                <Col offset={5}><Button type="primary" onClick={this.openCamera}>使用摄像头</Button></Col>
+                            </Row>
                         </Col>
                     </Row>
                 </section>
@@ -99,6 +235,10 @@ class ReactMeme extends PureComponent {
         )
     }
     componentDidMount() {
+        Modal.info({
+            title:"开发中...",
+            content:"敬请期待"
+        })
     }
 }
 
